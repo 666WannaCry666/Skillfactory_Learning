bb523936cbe58855358e5155b4570ec381b3b894
666WannaCry666 <62420233+666WannaCry666@users.noreply.github.com>
2021-03-14 22:46:51 +0700

    Delete testRectangle.py

--- Cat/testRectangle.py
+++
@@ -1,7 +0,0 @@
-from Cat import Cat
-
-baron = Cat("Барон", "Мальчик", 2)
-sam = Cat("Сэм", "Мальчик", 2)
-
-print(baron.name, baron.gender, baron.age, "года")
-print(sam.name, sam.gender, sam.age, "года")
