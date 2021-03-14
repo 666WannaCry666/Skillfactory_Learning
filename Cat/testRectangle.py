b508a7ee0626b4a53097dc25a4fc7accf580f961
666WannaCry666 <62420233+666WannaCry666@users.noreply.github.com>
2021-03-14 21:08:50 +0700

    Add files via upload

---
+++ Cat/testRectangle.py
@@ -0,0 +1,7 @@
+from Cat import Cat
+
+baron = Cat("Барон", "Мальчик", 2)
+sam = Cat("Сэм", "Мальчик", 2)
+
+print(baron.name, baron.gender, baron.age, "года")
+print(sam.name, sam.gender, sam.age, "года")
